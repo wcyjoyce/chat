0008ad2c51d17193f2aa20d9083f8993927cee4e
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-03-01 13:01:59 +0800

    destructured props, message content displayed, time formatted

--- src/components/message.jsx
+++ src/components/message.jsx
@@ -4,9 +4,15 @@ import { connect } from "react-redux";
 
 class Message extends Component {
   render() {
+    const { message } = this.props;
+    const time = new Date(message.created_at).toLocaleTimeString()
     return (
       <div className="message">
-        {this.props.message.author}
+        <div className="message-header">
+          <div className="message-author">{message.author}</div>
+          <div className="message-datetime">{time}</div>
+        </div>
+        <div className="message-content"> {message.content}</div>
       </div>
     );
   }

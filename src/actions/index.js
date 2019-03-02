dc1241d575149e0fe133a8983ae2b81e6a686bd9
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-03-02 11:31:16 +0800

    posting messages action done (though messages still appear when switching channels)

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,12 +6,30 @@ export function fetchMessages(channel) {
   return {
     type: "FETCH_MESSAGES",
     payload: promise
-  }
+  };
 }
 
 export function selectChannel(channel) {
   return {
     type: "SELECTED_CHANNEL",
     payload: channel
-  }
+  };
+}
+
+export function createMessage(channel, author, content) {
+  const url = `${base_url}/{channel}/messages`;
+  const body = { author, content };
+  const promise = fetch(url, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(body)
+  }).then(response => response.json());
+
+  return {
+    type: "POST_MESSAGE",
+    payload: promise
+  };
 }
--- src/reducers/messages_reducer.js
+++ src/reducers/messages_reducer.js
@@ -8,6 +8,11 @@ const messagesReducer = (state = null, action) => {
     case "SELECTED_CHANNEL": {
       return []; // clears view when selected channel has changed
     }
+    case "POST_MESSAGE": {
+      const messageList = state.slice(0);
+      messageList.push(action.payload);
+      return messageList;
+    }
     default:
       return state;
   }

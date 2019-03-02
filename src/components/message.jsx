2a6f9fb9adaad17e17c2a1542749d23631ead896
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-03-02 13:55:06 +0800

    added date to message

--- src/components/message.jsx
+++ src/components/message.jsx
@@ -1,16 +1,15 @@
 import React, { Component } from "react";
-import { bindActionCreators } from "redux";
-import { connect } from "react-redux";
 
 class Message extends Component {
   render() {
     const { message } = this.props;
+    const date = new Date(message.created_at).toDateString()
     const time = new Date(message.created_at).toLocaleTimeString()
     return (
       <div className="message">
         <div className="message-header">
           <div className="message-author">{message.author}</div>
-          <div className="message-datetime">{time}</div>
+          <div className="message-datetime">{date} {time}</div>
         </div>
         <div className="message-content"> {message.content}</div>
       </div>

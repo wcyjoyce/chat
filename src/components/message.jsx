83208dea8cf09cd5e37ef163b48bb99297f9fe4e
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-03-02 14:14:24 +0800

    further destructured props

--- src/components/message.jsx
+++ src/components/message.jsx
@@ -2,16 +2,17 @@ import React, { Component } from "react";
 
 class Message extends Component {
   render() {
-    const { message } = this.props;
-    const date = new Date(message.created_at).toDateString()
-    const time = new Date(message.created_at).toLocaleTimeString()
+    const { author, content, created_at } = this.props.message;
+    const date = new Date(created_at).toDateString()
+    const time = new Date(created_at).toLocaleTimeString()
+
     return (
       <div className="message">
         <div className="message-header">
-          <div className="message-author">{message.author}</div>
+          <div className="message-author">{author}</div>
           <div className="message-datetime">{date} {time}</div>
         </div>
-        <div className="message-content"> {message.content}</div>
+        <div className="message-content"> {content}</div>
       </div>
     );
   }
